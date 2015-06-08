89f08b8bcc69e6947e1a41f920212c5a17ce638b
johnnyli@live.ca <johnny.li@live.ca>
2015-06-08 18:08:23 -0400

    To do tutorial

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,19 @@
  */
 angular.module('webPortfolioApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.todos = [
+      'Item 1',
+      'Item 2',
+      'Item 3',
+      'Item 4'
     ];
+    $scope.addTodo = function () {
+      if ($scope.todos.indexOf($scope.todo) === -1) {
+        $scope.todos.push($scope.todo);
+        $scope.todo = '';
+      }
+    };
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    }
   });
